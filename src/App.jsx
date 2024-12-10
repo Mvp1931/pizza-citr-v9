5615c06700f5ee0de2e0585bd103dd6d88ad91ff
Mvp1931 <mihirphadnis02@gmail.com>
2024-12-10 13:15:44 +0530

    first setup citr-v9

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { createRoot } from "react-dom/client";
+import Pizza from "./Pizza.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Padre Gino's Pizza – Order Now</h1>
+      <Pizza name="Cheese Pizza" description="It's soooo much cheese" />
+      <Pizza
+        name="Pepperoni Pizza"
+        description="Mozzarella cheese, pepperoni, and onions"
+      />
+      <Pizza
+        name="Hawaiian Pizza"
+        description="Sliced ham, pineapple, and mozzarella cheese"
+      />
+      <Pizza
+        name="The Big Meat Pizza"
+        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+      />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
